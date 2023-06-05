c12929d1f5203cfa7931911cba8896e8e35110b8
IamSuruchiSharma <suru2772chisharma@gmail.com>
2023-06-05 12:46:47 +0530

    Changes

--- Chapter 3/Practiceset3.js
+++ Chapter 3/Practiceset3.js
@@ -2,21 +2,26 @@
 // Write a program to print the marks of a student in an object using for loop
 // obj={harry:98,rohan:70,suruchi:89}
 // let obj = {
-//     Harry: 98,
-//     Rohan: 78,
-//     Suruchi:89
-// }
-
-// for (let i = 0; i < Object.keys(marks).length; i++) {
-//     console.log("The marks of " + Object.keys(marks)[i] + " are " + marks[Object.keys(marks)[i]])
-// }
-
+    //     Harry: 98,
+    //     Rohan: 78,
+    //     Suruchi:89
+    // }
+    
+    // for (let i = 0; i < Object.keys(marks).length; i++) {
+        //     console.log("The marks of " + Object.keys(marks)[i] + " are " + marks[Object.keys(marks)[i]])
+        // }
+        
+//Question - 02
+// Write a program to print the marks of a student in an object using for in loop
+// obj={harry:98,rohan:70,suruchi:89}
 // for (let a in obj) {
-//     console.log("Marks of", a, ":", obj[a]);
-// }
-
+    //     console.log("Marks of", a, ":", obj[a]);
+    // }
+    
+    
+//Question - 02
+// Yadi koi vyakti galat number enter kre to usko try again msg show kriye thik hai esa program bnao.
 // let num=prompt("Enter the number ");
-
 // if(num!=123456){
 //     while(num!=123456){
 //         if(num<123456){
@@ -30,10 +35,11 @@
 //     }
 // }
 
-//Question - 04
-//Write a program to find mean of five no
-let num1 = 23, num2 = 26, num3 = 45, num4 = 29, num5 = 30;
-let mean = (a, b, c, d, e)=>{
-    return ((a + b + c + d + e)/5);
-}
-console.log("Mean of five number is :",mean(num1, num2, num3, num4, num5));
+
+// Question - 04
+// Write a program to find mean of five no
+// let num1 = 23, num2 = 26, num3 = 45, num4 = 29, num5 = 30;
+// let mean = (a, b, c, d, e)=>{
+//     return ((a + b + c + d + e)/5);
+// }
+// console.log("Mean of five number is :",mean(num1, num2, num3, num4, num5));
